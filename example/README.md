9829b72c211b1c00e51a5c09ef31a2dd54f7164c
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2022-08-28 23:07:23 +0100

    add github workflow and example

---
+++ example/README.md
@@ -0,0 +1,11 @@
+# How to start example page?
+
+Then in the example directory, run all dependencies:
+
+### `yarn install`
+
+Then use the command:
+
+### `yarn start`
+
+Open [http://localhost:1234](http://localhost:1234) to view it in the browser.
---
+++ example/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Calendar } from 'reactjs-availability-calendar'
+
+const bookings = [
+  {
+    from: '01-08-2022',
+    to: '01-16-2022',
+    middayCheckout: true,
+  },
+  {
+    from: '06-25-2022',
+    to: '07-03-2022',
+    middayCheckout: false,
+  },
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <div className='demo'>
+      <Calendar bookings={bookings} />
+    </div>
+  </React.StrictMode>,
+)
